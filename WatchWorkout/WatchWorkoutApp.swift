38317fc63970b4f66b73ba6b7bf65ae20a2a93b6
Ben Gottlieb <ben@standalone.com>
2021-07-31 04:53:43 -0500

    Initial Commit

---
+++ WatchWorkout/WatchWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchWorkoutApp.swift
+//  WatchWorkout
+//
+//  Created by Ben Gottlieb on 7/31/21.
+//
+
+import SwiftUI
+
+@main
+struct WatchWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
